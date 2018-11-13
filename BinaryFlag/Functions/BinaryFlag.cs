a52f9687102908ce8ff067df9d3d6c31337fa336
Dzmitry Dym <ddym@thinkcompass.io>
2018-11-13 12:02:03 +0200

    Help methods to get or set flag by int.

--- BinaryFlag/Functions/BinaryFlag.cs
+++ BinaryFlag/Functions/BinaryFlag.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.SqlServer.Server;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
+using System.Linq;
 
 namespace BinaryFlag.Functions
 {
@@ -25,7 +27,7 @@ namespace BinaryFlag.Functions
             }
         }
 
-        public static byte[] SetBinaryFlag(int index, bool flag, byte[] sqlBytes = null)
+        public static byte[] SetBinaryFlag(int index, bool flag, byte[] sqlBytes = null, bool cleanTail = true)
         {
             if (index < 1)
                 throw new IndexOutOfRangeException("Index cannot be less than zero or negative.");
@@ -52,7 +54,7 @@ namespace BinaryFlag.Functions
             else
                 bytes[byteIndex] = (byte)(bytes[byteIndex] & ~(byte)Math.Pow(2, (index - 1) - (byteIndex * 8)));
 
-            if (!flag && bytes.Length > 0 && bytes[bytes.Length - 1] == 0)
+            if (cleanTail && !flag && bytes.Length > 0 && bytes[bytes.Length - 1] == 0)
                 for (int i = bytes.Length - 1; i > -1; i--)
                     if (bytes[i] > 0)
                     {
@@ -98,6 +100,35 @@ namespace BinaryFlag.Functions
 
             return (sqlBytes[byteIndex] &
                 (byte)Math.Pow(2, (index - 1) - (byteIndex * 8))) != 0;
+        }
+
+        public static IEnumerable<int> FindBinaryIndexes(byte[] sqlBytes)
+        {
+            if (sqlBytes == null)
+                throw new ArgumentNullException(nameof(sqlBytes));
+
+            for (int i = 0; i < sqlBytes.Length; i++)
+                for (int b = 1; b < 9; b++)
+                {
+                    int index = (i * 8) + b;
+                    if (HasBinaryFlag(index, sqlBytes))
+                        yield return index;
+                }
+        }
+
+        public static byte[] MarkBinaryIndexes(IEnumerable<int> indexes)
+        {
+            if (indexes == null)
+                throw new ArgumentNullException(nameof(indexes));
+
+            int count = indexes.Count();
+            int bytesLength = (int)Math.Ceiling(count / 8f);
+            byte[] bytes = new byte[1];
+
+            foreach (int index in indexes)
+                bytes = SetBinaryFlag(index, true, bytes, false);
+
+            return bytes;
         }
     }
 }
