ba9fe3126d31e5b52c4eda886e688847b19239a2
Dzmitry Dym <devolick@gmail.com>
2018-11-13 19:39:54 +0200

    Changes for nuget.

--- BinaryFlag/Properties/AssemblyInfo.cs
+++ BinaryFlag/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,12 +7,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BinaryFlag")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Small plugin for SQL Management Studio / CSharp for big binary data to set and check flag.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dzmitry Dym")]
 [assembly: AssemblyProduct("BinaryFlag")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("DDM")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
